30ed393144bdb736b1e62b0fd62867e98cb8d93a
0xAnon <utkarshshail005@protonmail.com>
2023-11-20 01:55:25 +0530

    News tests added

--- test/test.js
+++ test/test.js
@@ -29,6 +29,8 @@ describe('GPURentalMarketplace', () => {
         it("Is properly deployed", async() => {
 
             const {ProxyV1,owner} = await mainDeploy()
+
+            // Check owner is initialized 
             const testOwner  = await ProxyV1.owner()
             expect(testOwner).to.equal(owner.address)
 
@@ -42,11 +44,13 @@ describe('GPURentalMarketplace', () => {
             // console.log(`Main contract and Proxy Deployed`)
             // await sleep(5*1000)
 
+            // Deployed new contract logic
             const GPURentalMarketplaceV2 = await hre.ethers.deployContract("GPURentalMarketplaceV2")
             await GPURentalMarketplaceV2.waitForDeployment()
 
             // console.log(`V2 contract Deployed`)
 
+            // Update contract login in old proxy contract
             const upgrade = ProxyV1.updateCode(GPURentalMarketplaceV2.target)
             const ProxyV2 = await GPURentalMarketplaceV2.attach(ProxyV1.target);
 
@@ -101,11 +105,9 @@ describe('GPURentalMarketplace', () => {
 
         })
 
-
         it("Register Machine Check", async() => {
 
-            const {ProxyV1,owner,otherAccount} = await mainDeploy()
-
+            const {ProxyV1,owner} = await mainDeploy()
             await ProxyV1.registerUser("0xanon", 100000, "test", owner.address)
 
             // Unauthorized call 
@@ -156,6 +158,82 @@ describe('GPURentalMarketplace', () => {
             expect(machineInfo[0]).to.equal("AMD EPYC 7R32")
 
         })
+
+        it("Rent Machine Check", async() => {
+
+            const {ProxyV1,owner} = await mainDeploy()
+            await ProxyV1.registerUser("0xanon", 100000, "test", owner.address)
+
+            // Keys Setup
+            await ProxyV1.setKeys(
+                owner.address,
+                "0xcE408f35c3D43F5609151310309De73f3e57Ec76",
+                "0x6e54ebe8067bE3dc516D8a14bb40f4224b83FB46"
+            )
+
+            // Register Machine
+            await ProxyV1.registerMachines("AMD EPYC 7R32", "NVIDIA A10G",22,10,512,8,"3.235.148.209",[22,80],"Asia",5,owner.address)
+
+
+            // Gpoint Fallback check
+            await expect(
+                ProxyV1.rentMachine(
+                    10001,
+                    2,
+                    101
+                )
+            )
+            .to.be.revertedWith("Not enough Gpoints");
+
+            // Event Test
+            await expect(
+                ProxyV1.rentMachine(
+                    10001,
+                    1,
+                    101
+                )
+            )
+            .to.emit(ProxyV1, "MachineRented")
+            .withArgs(1, 10001, owner.address);
+
+        })
+
+        it("Complete Order Check", async() => {
+
+            const {ProxyV1,owner} = await mainDeploy()
+            await ProxyV1.registerUser("0xanon", 100000, "test", owner.address)
+
+            // Keys Setup
+            await ProxyV1.setKeys(
+                owner.address,
+                "0xcE408f35c3D43F5609151310309De73f3e57Ec76",
+                "0x6e54ebe8067bE3dc516D8a14bb40f4224b83FB46"
+            )
+
+            // Register Machine
+            await ProxyV1.registerMachines("AMD EPYC 7R32", "NVIDIA A10G",22,10,512,8,"3.235.148.209",[22,80],"Asia",1,owner.address)
+
+            // Rent Machine
+            await ProxyV1.rentMachine(10001,2,101)
+
+            // Check complete order before order
+            await expect(
+                ProxyV1.completeOrder(1)
+            )
+            .to.be.revertedWithoutReason()
+
+            // Set the next block timestamp to end order timestamp
+            const currentTimestamp = (await ethers.provider.getBlock()).timestamp;
+            await ethers.provider.send("evm_setNextBlockTimestamp", [currentTimestamp + 3600*2]);
+
+            await ProxyV1.completeOrder(1)
+
+            // Check if order is completed
+            const orderInfo = await ProxyV1.orders(1)
+            expect(orderInfo.isPending).to.equal(false)
+
+        })
+
 
     })
 
